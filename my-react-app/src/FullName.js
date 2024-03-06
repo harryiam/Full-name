c9affbbf0b1e4f9a96611d78cd95f8cd592f5f1f
AMBATI HARITHA <ambatiharitha@AMBATIs-MacBook-Air.local>
2024-03-06 15:22:41 +0530

    fullname

--- my-react-app/src/FullName.js
+++ my-react-app/src/FullName.js
@@ -10,40 +10,38 @@ function UserForm() {
     if (firstName.trim() && lastName.trim()) {
       const fullNameValue = `${firstName} ${lastName}`;
       setFullName(fullNameValue);
-      setFirstName('');
-      setLastName('');
+      // Do not reset the input fields if the form is submitted successfully
     }
   };
 
   return (
     <>
-    <h3>Full Name Display</h3>
-    
-    <form onSubmit={handleSubmit}>
-      <div>
-      <label>First:</label>
-      <input
-        type="text"
-        value={firstName}
-        onChange={(e) => setFirstName(e.target.value)}
-        required
-      />
-      </div>
-      <div>
-      <label>Last:</label>
-      <input
-        type="text"
-        value={lastName}
-        onChange={(e) => setLastName(e.target.value)}
-        required
-      />
-      </div>
-      <button type="submit">Submit</button>
-      {fullName && <div>Full Name: {fullName}</div>}
-    </form>
+      <h3>Full Name Display</h3>
+      
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>First:</label>
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Last:</label>
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+        {fullName && <div>{`Full Name: ${fullName}`}</div>}
+      </form>
     </>
   );
-  
 }
 
 export default UserForm;
