ea1448c575f682adbdb1f6e634f75fd4f066edc6
AMBATI HARITHA <ambatiharitha@AMBATIs-MacBook-Air.local>
2024-03-06 15:05:37 +0530

    fullname

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import FullName from './FullName';
+import UserForm from './FullName';
 
 function App() {
   return (
     <div className="App">
-      <FullName/>
+      <UserForm/>
     </div>
   );
 }
---
+++ my-react-app/src/FullName.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+function UserForm() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [fullName, setFullName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName.trim() && lastName.trim()) {
+      const fullNameValue = `${firstName} ${lastName}`;
+      setFullName(fullNameValue);
+      setFirstName('');
+      setLastName('');
+    }
+  };
+
+  return (
+    <>
+    <h3>Full Name Display</h3>
+    
+    <form onSubmit={handleSubmit}>
+      <div>
+      <label>First:</label>
+      <input
+        type="text"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+        required
+      />
+      </div>
+      <div>
+      <label>Last:</label>
+      <input
+        type="text"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+        required
+      />
+      </div>
+      <button type="submit">Submit</button>
+      {fullName && <div>Full Name: {fullName}</div>}
+    </form>
+    </>
+  );
+  
+}
+
+export default UserForm;
